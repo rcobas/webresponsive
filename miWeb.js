4be7ad4554f509c2c5da8a6aba56a345f0181daa
newcodertrans <47862774+newcodertrans@users.noreply.github.com>
2019-12-03 23:37:09 +0100

    First Commit

---
+++ miWeb.js
@@ -0,0 +1,8 @@
+function mostrarMenu() {
+  var menu = document.getElementById("items-menu");
+  menu.classList.add("open");
+}
+function cerrarMenu() {
+  var menu = document.getElementById("items-menu");
+  menu.classList.remove("open");
+}
