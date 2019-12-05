1c191a4fc0a49563944e782d32f8479a4cc0efd6
newcodertrans <47862774+newcodertrans@users.noreply.github.com>
2019-12-05 10:49:07 +0100

    webfinal

--- miWeb.js
+++ miWeb.js
@@ -6,3 +6,31 @@ function cerrarMenu() {
   var menu = document.getElementById("items-menu");
   menu.classList.remove("open");
 }
+
+function mensajeEnviar() {
+  var bt = document.getElementById("bt-enviar");
+
+  alert("Su mensaje se ha enviado.");
+}
+
+function myFocusFunction() {
+  document.getElementById("enlace").style.color = "purple";
+}
+
+function dobleClick() {
+  document.getElementById("td-morado").style.backgroundColor =
+    "rgb(211, 222, 247)";
+  document.getElementById("td-verde").style.backgroundColor =
+    " rgb(232, 250, 226)";
+  document.getElementById("td-rosa").style.backgroundColor =
+    " rgb(250, 226, 248)";
+}
+
+var input = document.getElementById("buscador");
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+
+    document.getElementById("buscador").click();
+  }
+});
